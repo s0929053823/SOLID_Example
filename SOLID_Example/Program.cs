89191939ddfc8861a1372274feeaab2352d25d90
NickLiu <stalinmalcontent2009@gmail.com>
2019-05-13 15:21:38 +0800

    加入專案檔案。

---
+++ SOLID_Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SOLID_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+    
