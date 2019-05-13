fbe27f4ac3ea77ec07ad995a6e44baafe064f817
NickLiu <stalinmalcontent2009@gmail.com>
2019-05-13 16:12:38 +0800

    delete project

---
+++ SOLID_Example/Models/Product/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID_Example.Models.Product
+{
+    public interface IProductRepository
+    {
+        IEnumerable<ProductModel> GetByFileName(string fileName);
+    }
+}
---
+++ SOLID_Example/Models/Product/ProductPresenter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gtk;
+
+namespace SOLID_Example.Models.Product
+{
+    public class ProductPresenter
+    {
+
+        private readonly FileChooserDialog openFileDialog;
+        private readonly IProductRepository repository;
+        private readonly IProductView view;
+
+
+        public ProductPresenter()
+        {
+            //view = new Program();
+            //view.Initialize();
+            //repository = new ProductRepository();
+            //openFileDialog = new FileChooserDialog();
+        }
+
+        public void BrowseForFileName()
+        { 
+
+        }
+
+        public IEnumerable<ProductModel> GetProducts()
+        {
+        return new List<ProductModel>();
+        }
+    }
+}
