afa062a80fa196d22f2220285bf2298e17dc30f5
NickLiu <stalinmalcontent2009@gmail.com>
2019-05-13 16:31:59 +0800

    add .NET MVC Core Prokect"

---
+++ SOLID_Web/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SOLID_Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+    }
+}
